34b08d2b9ee4386aac462812d7d0428ea95a67bb
silvester-pari <silvester.pari@eox.at>
2024-11-19 23:05:56 +0100

    feat: allow rendering narrative preview

--- .vitepress/theme/Narrative.vue
+++ .vitepress/theme/Narrative.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import { useData } from "vitepress";
 const { page } = useData();
+let fetchedContent = ref("");
+onMounted(async () => {
+  if (page.value.relativePath?.includes("preview")) {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("url")) {
+      const response = await fetch(urlParams.get("url"));
+      fetchedContent.value = await response.text();
+    }
+  }
+});
 </script>
 
 <template>
   <ClientOnly>
-    <eox-storytelling .markdown="page.content"></eox-storytelling>
+    <eox-storytelling
+      show-nav
+      .markdown="page.content || fetchedContent"
+    ></eox-storytelling>
   </ClientOnly>
 </template>
 
@@ -13,5 +27,7 @@ const { page } = useData();
 eox-storytelling {
   --header-font-family: Poppins, sans-serif;
   --body-font-family: Poppins, sans-serif;
+  margin: calc(var(--vp-nav-height) * -1) 0 var(--vp-nav-height) 0;
+  transform: translateY(var(--vp-nav-height));
 }
 </style>
