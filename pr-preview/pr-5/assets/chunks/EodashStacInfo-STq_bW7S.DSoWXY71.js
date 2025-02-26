8077842fd6ebec9f7c77d53e950ee3da645172d0
santilland <santilland@users.noreply.github.com>
2025-02-26 15:27:09 +0000

    Deploy preview for PR 5 🛫

--- pr-preview/pr-5/assets/chunks/EodashStacInfo-STq_bW7S.DSoWXY71.js
+++ pr-preview/pr-5/assets/chunks/EodashStacInfo-STq_bW7S.DSoWXY71.js
@@ -1,4 +1,4 @@
-import"./main.DLDzFRqc.js";import{Q as t}from"./eo-dash.kYJsMtil.js";import{c as r,o,j as a,k as i}from"./framework.54qQzOw3.js";import"./lit-element.Deg-YTNa.js";import"./directive.qnhzJN6s.js";import"./commonjsHelpers.BosuxZz1.js";import"./map.ZocK5lBO.js";const l={class:"flex-grow-1 fill-height overflow-auto"},n=[".for",".allowHtml",".body",".featured",".footer",".styleOverride",".header",".subheader",".tags"],g={__name:"EodashStacInfo",props:{allowHtml:{type:Boolean,default:!0},styleOverride:{type:String,default:`
+import"./main.DLDzFRqc.js";import{Q as t}from"./eo-dash.GiWabc8F.js";import{c as r,o,j as a,k as i}from"./framework.54qQzOw3.js";import"./lit-element.Deg-YTNa.js";import"./directive.qnhzJN6s.js";import"./commonjsHelpers.BosuxZz1.js";import"./map.ZocK5lBO.js";const l={class:"flex-grow-1 fill-height overflow-auto"},n=[".for",".allowHtml",".body",".featured",".footer",".styleOverride",".header",".subheader",".tags"],g={__name:"EodashStacInfo",props:{allowHtml:{type:Boolean,default:!0},styleOverride:{type:String,default:`
 .single-property {columns: 1!important;}
 h1 {margin:0px!important;font-size:16px!important;}
 header h1:after {
