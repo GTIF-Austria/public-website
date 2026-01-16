a14023f4b78810e832f38854a6178e4c344a361f
lubojr <lubojr@seznam.cz>
2026-01-16 10:04:56 +0000

    Deploying to gh-pages from @ GTIF-Austria/public-website@fb9d7870daec5222aacb45c80a39160205c9837a 🚀

---
+++ assets/chunks/ssrBoot-Zgc_Ttvi.B2N5AeW1.js
@@ -0,0 +1,1 @@
+import{D as s,v as t,as as e,S as n}from"./framework.bPhVVXWZ.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
