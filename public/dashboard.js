b80934b7a8bab68174d3af6505e5fb11c0d61566
Lubomír Doležal <lubomir.dolezal@eox.at>
2026-09-22 13:56:25 +0200

    fix: increase sizes of panels to fill available space better (#31)
    * fix: increase sizes of panels to fill available space better
    
    * fix: disable geolocation
    
    * fix: syntax

--- public/dashboard.js
+++ public/dashboard.js
@@ -4,7 +4,9 @@ function isMobile() {
   return window.innerWidth < minWidth || screen.width < minWidth;
 }
 const cacheBuster = `?t=${new Date().getTime()}`; // Add a timestamp for cache busting
-const feedbackSchema = await fetch(`/configs/feedback_schema.json${cacheBuster}`).then(res => res.json());
+const feedbackSchema = await fetch(
+  `/configs/feedback_schema.json${cacheBuster}`,
+).then((res) => res.json());
 
 const itemFilterConfig = {
   resultType: "cards",
@@ -101,6 +103,7 @@ export default {
               },
               enableBackToPOIs: true,
               enableGlobe: false,
+              enableGeolocation: false,
             },
             btnsPosition: {
               x: "12/9/10",
@@ -131,7 +134,7 @@ export default {
           id: "Layers",
           type: "internal",
           title: "Layers",
-          layout: { x: 0, y: 1, w: "3/3/2", h: 6 },
+          layout: { x: 0, y: 1, w: "3/3/2", h: 10 },
           widget: {
             name: "EodashLayerControl",
             properties: {
@@ -156,7 +159,10 @@ export default {
                       featured: [
                         "providers",
                         { key: "eodash:narratives" },
-                        { key: "assets", filter: (asset) => !(asset?.roles?.includes("story"))}
+                        {
+                          key: "assets",
+                          filter: (asset) => !asset?.roles?.includes("story"),
+                        },
                       ],
                     },
                   },
@@ -171,7 +177,7 @@ export default {
                   id: "Processes",
                   type: "internal",
                   title: "Processes",
-                  layout: { x: "9/9/10", y: 6, w: "3/3/2", h: 5 },
+                  layout: { x: "9/9/10", y: 6, w: "3/3/2", h: 6 },
                   widget: {
                     name: "EodashProcess",
                   },
@@ -184,7 +190,7 @@ export default {
               id: "ProcessResultChart",
               type: "internal",
               title: "Chart",
-              layout: { x: 0, y: 0, w: 12, h: 8 },
+              layout: { x: 0, y: 0, w: 12, h: 9 },
               widget: {
                 name: "EodashChart",
               },
@@ -228,6 +234,7 @@ export default {
                 countrycode: "at",
               },
               enableGlobe: false,
+              enableGeolocation: false,
             },
             btnsPosition: {
               x: "12/9/10",
@@ -272,32 +279,54 @@ export default {
           },
         },
         {
-          id: "Layers",
-          type: "internal",
-          title: "Layers",
-          layout: { x: 0, y: 1, w: "3/3/2", h: 6 },
-          widget: {
-            name: "EodashLayerControl",
-            properties: {
-              cssVars: {
-                "--list-padding": "1rem",
+          defineWidget: (selectedSTAC) => {
+            return {
+              id: "Layers",
+              type: "internal",
+              title: "Layers",
+              layout: {
+                x: 0,
+                y: 1,
+                w: "3/3/2",
+                h: window.eodashStore.actions.includesProcess(selectedSTAC)
+                  ? 5
+                  : 11,
               },
-            },
+              widget: {
+                name: "EodashLayerControl",
+                properties: {
+                  cssVars: {
+                    "--list-padding": "1rem",
+                  },
+                },
+              },
+            };
           },
         },
         {
-          id: "Layers Compare",
-          title: "Comparison Layers",
-          layout: { x: "9/9/10", y: 1, w: "3/3/2", h: 6 },
-          type: "internal",
-          widget: {
-            name: "EodashLayerControl",
-            properties: {
-              map: "second",
-              cssVars: {
-                "--list-padding": "1rem",
+          defineWidget: (selectedSTAC) => {
+            return {
+              id: "Layers Compare",
+              title: "Comparison Layers",
+              layout: {
+                x: "9/9/10",
+                y: 1,
+                w: "3/3/2",
+                h: window.eodashStore.actions.includesProcess(selectedSTAC)
+                  ? 5
+                  : 11,
               },
-            },
+              type: "internal",
+              widget: {
+                name: "EodashLayerControl",
+                properties: {
+                  map: "second",
+                  cssVars: {
+                    "--list-padding": "1rem",
+                  },
+                },
+              },
+            };
           },
         },
         {
@@ -307,7 +336,7 @@ export default {
                   id: "Processes",
                   type: "internal",
                   title: "Processes",
-                  layout: { x: 0, y: 7, w: "3/3/2", h: 5 },
+                  layout: { x: 0, y: 6, w: "3/3/2", h: 6 },
                   widget: {
                     name: "EodashProcess",
                   },
@@ -323,7 +352,7 @@ export default {
               id: "CompareMapProcess",
               type: "internal",
               title: "Processes",
-              layout: { x: "9/9/10", y: 7, w: "3/3/2", h: 5 },
+              layout: { x: "9/9/10", y: 6, w: "3/3/2", h: 6 },
               widget: {
                 name: "EodashProcess",
                 properties: {
@@ -338,7 +367,7 @@ export default {
               id: "ProcessResultChart",
               type: "internal",
               title: "Chart",
-              layout: { x: 0, y: 0, w: 6, h: 8 },
+              layout: { x: 0, y: 0, w: 6, h: 9 },
               widget: {
                 name: "EodashChart",
               },
@@ -350,7 +379,7 @@ export default {
               id: "ProcessResultChartCompare",
               type: "internal",
               title: "Compare Chart",
-              layout: { x: 6, y: 0, w: 6, h: 8 },
+              layout: { x: 6, y: 0, w: 6, h: 9 },
               widget: {
                 name: "EodashChart",
                 properties: {
